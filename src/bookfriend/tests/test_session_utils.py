a1e248372dc4d9771c2d7138a4575bfc147fff6c
Omar Shaikh <senseofomar@gmail.com>
2026-02-01 15:48:25 +0530

    testing

--- src/bookfriend/tests/test_session_utils.py
+++ src/bookfriend/tests/test_session_utils.py
@@ -4,17 +4,7 @@ from bookfriend.utils import session_utils
 
 TEST_PATH = "test_session.json"
 
-def test_save_and_load_session():
-    data ={"food":"bar", "count": 124}
-    session_utils.save_session(data, TEST_PATH)
 
-    assert Path(TEST_PATH).exists()    #old school way os.path.exists(TEST_PATH)
-
-    loaded = session_utils.load_session(TEST_PATH)
-
-    assert loaded == data
-
-    Path(TEST_PATH).unlink()
 
 def test_reset_session_removes_file(tmp_path):
     # Arrange: create a fake session file
